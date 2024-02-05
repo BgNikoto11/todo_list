1d5ef99a4dd252e780f3b6433672d39dfc8ed7c3
BgNkoto11 <nikolamilanov2005@gmail.com>
2024-02-05 14:23:28 +0200

    added list-item buttons

--- script.js
+++ script.js
@@ -11,9 +11,19 @@ function addTodo(){
 
         let todoItemText = document.createElement("p");
         todoItemText.textContent = todoText;
+
+        let completeButton = document.createElement("button");
+        completeButton.className = "complete-button";
+        completeButton.textContent = "Completed";
+
+        let deleteButton = document.createElement("button");
+        deleteButton.className = "delete-button";
+        deleteButton.textContent = "Remove";
        
         todoList.appendChild(todoItem);
         todoItem.appendChild(todoItemText);
+        todoItem.appendChild(completeButton);
+        todoItem.appendChild(deleteButton);
 
         todoInput.vale = ""
     }
